09a890c55472e9c1a343a9a2bacf11032623f52a
Donte Handy <donte.handy@gmail.com>
2025-01-16 21:21:21 -0400

    add count_of_games_by_season method and corresponding tests

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -149,4 +149,10 @@ class StatTracker
     end
   end
 
+  def count_of_games_by_season
+    games_by_season = Hash.new(0)
+    @games.each { |game| games_by_season[game[:season]] += 1 }
+    games_by_season
+  end
+
 end
